fba7722a580d41774a87d3a86688ebdcae712a83
Liyina Veizaga <liyina.veizaga@swissbytes.ch>
2018-06-13 23:13:12 -0400

    prepare client main number2Lcd

--- tdd/int2lcd/src/main/java/edu/alsie/tdd/int2lcd/Main.java
+++ tdd/int2lcd/src/main/java/edu/alsie/tdd/int2lcd/Main.java
@@ -1,4 +1,16 @@
 package edu.alsie.tdd.int2lcd;
 
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Int2lcd int2lcd = new Int2lcd();
+        List<String> number2Lcd = int2lcd.convertNumber2Lcd(123456789);
+        System.out.println(number2Lcd.get(0));
+        System.out.println(number2Lcd.get(1));
+        System.out.println(number2Lcd.get(2));
+    }
+
+
 }
