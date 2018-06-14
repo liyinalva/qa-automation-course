45c5cc86500adfca21c0c0b99eb483e13767f854
Liyina Veizaga <liyina.veizaga@swissbytes.ch>
2018-06-13 20:49:16 -0400

    [tdd/Liyina/Int2Lcd] Add intro to int2lcd

---
+++ tdd/int2lcd/src/test/java/edu/alsie/tdd/int2lcd/Int2lcdTest.java
@@ -0,0 +1,14 @@
+package edu.alsie.tdd.int2lcd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Int2lcdTest {
+
+    @Test
+    public void testIfAnumberIsDigitOne() {
+        Int2lcd int2lcd = new Int2lcd();
+        Assert.assertTrue(int2lcd.isDigitOne(1));
+    }
+
+}
---
+++ tdd/int2lcd/src/test/java/edu/alsie/tdd/int2lcd/MainTest.java
@@ -0,0 +1,14 @@
+package edu.alsie.tdd.int2lcd;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        assertThat(true, CoreMatchers.is(true));
+    }
+}
